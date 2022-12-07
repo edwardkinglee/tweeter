ab90c32da4236db952e920061fd1ba97bd73fd58
edwardkinglee <edward.lee3@telus.com>
2022-12-06 19:59:19 -0500

    Update counter to input event

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ console.log('composer file loaded');
 $(document).ready(function() {
   console.log('document ready started');
   
-  $("textarea").on('keyup', function() {
+  $("textarea").on('input', function() {
     let textUsed = $(this).val().length;
     const textLimit = 140;
     let count = textLimit - textUsed;
