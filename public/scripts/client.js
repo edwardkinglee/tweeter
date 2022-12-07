f3212dd684c3d3da04994370d9be493c1fedde43
edwardkinglee <edward.lee3@telus.com>
2022-12-07 02:28:11 -0500

    Add Ajax /tweets Get

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,6 @@
+
 /* eslint-disable no-undef */
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
 
   const renderTweets = function(tweets) {
 
@@ -65,9 +41,19 @@ $(document).ready(function() {
     return $tweet;
   };
   
+  loadTweets = () => {
+    
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function(tweets) {
+        console.log('load Tweets', tweets);
+        renderTweets(tweets);
+      });
+  };
+
+  loadTweets();
+  
   $('#tweet-form').on('submit', function(event) {
     event.preventDefault();
-    console.log('Button clicked, performing ajax call...');
     console.log($(this).serialize());
     const formData = $(this).serialize();
     $.ajax('/tweets', {
@@ -79,13 +65,4 @@ $(document).ready(function() {
       });
     return false;
   });
-
-  renderTweets(data);
-
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
 });
